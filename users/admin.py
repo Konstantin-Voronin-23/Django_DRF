cece0ee751bba7a9b9dcf635cc328f4240b9dd25
Konstantin Voronin <heavensmiting@gmail.com>
2025-09-25 00:14:44 +0300

    описаны сериализаторы платежей, урлы, вьюшки, админ панели, так же создана кастомная команда payments для загрузки данных в фикстуру

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Payment, User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["email", "phone", "sity"]
+    list_filter = ["sity"]
+    search_fields = ["email", "phone"]
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ["user", "payment_date", "amount", "payment_method"]
+    list_filter = ["payment_method", "payment_date"]
+    search_fields = ["user__email"]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, viewsets
 
-# Create your views here.
+from .models import Payment
+from .serializers import PaymentSerializer
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    """Класс описывающий вьюсет для платежей с фильтрацией"""
+
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ["paid_course", "paid_lesson", "payment_method"]
+    ordering_fields = ["payment_date", "amount"]
+    ordering = ["-payment_date"]
