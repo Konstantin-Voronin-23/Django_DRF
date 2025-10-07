62cb40c4f703a3273cbc721abe2b72786eb1eb75
Konstantin Voronin <heavensmiting@gmail.com>
2025-10-07 23:24:38 +0300

    подключена и настроена документация

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("lms/", include("lms.urls", namespace="lms")),
     path("users/", include("users.urls", namespace="users")),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
 ]
